9cb795c179f292187709fbeb1503ef0fb78992c4
tedawf <hello@tedawf.com>
2024-07-12 11:43:27 +0800

    add footer

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -3,3 +3,26 @@ export const baseData = {
   description:
     "Ted is a full stack developer with a passion for learning about new technologies.",
 };
+
+export const socialData = [
+  {
+    title: "email",
+    href: "mailto:hello@tedawf.com",
+    icon: "icon--email",
+  },
+  {
+    title: "github",
+    href: "https://github.com/tedawf",
+    icon: "icon--github",
+  },
+  {
+    title: "linkedin",
+    href: "https://linkedin.com/in/tedawf",
+    icon: "icon--linkedin",
+  },
+  {
+    title: "twitter",
+    href: "https://twitter.com/ted_awf",
+    icon: "icon--twitter",
+  },
+];
