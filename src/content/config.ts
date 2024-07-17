8582c262abe4d865b5a4e3e11a50a6a2f431c539
tedawf <hello@tedawf.com>
2024-07-17 23:07:48 +0800

    add posts collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -28,8 +28,27 @@ const projects = defineCollection({
     }),
 });
 
+const posts = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z
+        .string()
+        .max(65, { message: "Title is more than 65 characters" }),
+      description: z.string().max(165, {
+        message: "Description is more than 165 characters",
+      }),
+      image: image().refine((img) => img.width >= 1000, {
+        message: "Image is less than 1000px wide",
+      }),
+      pubDate: z.date(),
+      isDraft: z.boolean().optional(),
+    }),
+});
+
 export const collections = {
   points,
   tools,
   projects,
+  posts,
 };
